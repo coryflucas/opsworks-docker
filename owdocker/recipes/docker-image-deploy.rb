9db97afb8d32a1673f15ac527e7b2e3b3c8996f7
Chris Heisel <chris@heisel.org>
2014-11-24 15:57:31 -0500

    Revert "Roll my own registry login"
    This reverts commit a52234d52f5185d55f7d58b142c2a018b1b3c1f1.

--- owdocker/recipes/docker-image-deploy.rb
+++ owdocker/recipes/docker-image-deploy.rb
@@ -39,16 +39,10 @@ node[:deploy].each do |application, deploy|
   end
 
   Chef::Log.info("REGISTRY: Login as #{deploy[:environment_variables][:registry_username]} to #{deploy[:environment_variables][:registry_url]}")
-
-  bash "docker_registry" do
-    email = deploy[:environment_variables][:registry_email]
-    username = deploy[:environment_variables][:registry_username]
-    password = deploy[:environment_variables][:registry_password]
-    registry_url = deploy[:environment_variables][:registry_url]
-    user "root"
-    code <<-EOH
-      docker login -e="#{email}" -u="#{username}" -p="#{password}" #{registry_url}
-    EOH
+  docker_registry "#{deploy[:environment_variables][:registry_url]}" do
+    username deploy[:environment_variables][:registry_username]
+    password deploy[:environment_variables][:registry_password]
+    email deploy[:environment_variables][:registry_username]
   end
 
   # Pull tagged image
