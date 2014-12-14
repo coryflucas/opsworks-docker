ece4c9cf22a4af601b5384a653dba8ac986388d5
Cory Lucas <clucas@kabbage.com>
2014-12-14 15:53:58 -0500

    dont add the docker repo if its already added

--- owdocker/recipes/install.rb
+++ owdocker/recipes/install.rb
@@ -7,6 +7,7 @@ apt_repository "docker" do
   components ["main"]
   keyserver "hkp://keyserver.ubuntu.com:80"
   key "36A1D7869245C8950F966E92D8576A8BA88D21E9"
+  not_if { File.exists?("/etc/apt/sources.list.d/docker.list") }
 end
 
 # Install Docker latest version
